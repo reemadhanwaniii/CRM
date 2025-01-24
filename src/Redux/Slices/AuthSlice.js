10cf3079ad50d4141535c1c1d5b3a39174265d71
Reema Dhanwani <reemadhanwani1234@gmail.com>
2025-01-24 18:06:46 +0530

    Added auth slice

---
+++ src/Redux/Slices/AuthSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState = {
+    role: localStorage.getItem('role') || "",
+    data: localStorage.getItem('data') || {},
+    isLoggedIn: localStorage.getItem('isLoggedIn') || false
+}
+
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {}
+});
+
+
+export default authSlice.reducer;
