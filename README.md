499ad2c419668f16e7f5f0007c24ee6668a3baad
Reema Dhanwani <reemadhanwani1234@gmail.com>
2025-01-25 20:48:15 +0530

    update readme file

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+
+
+![alt text](image.png)
+
+![alt text](image-1.png)
+
+
+(ticket create with customer and assign to engineer whose status is approved and then login with engineer and see what all tickets are assigned)
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,12 +6,14 @@ import HomeLayout from "../../layouts/HomeLayout";
 function Home() {
     return(
        <HomeLayout>
+        <div className="mt-10 flex flex-row justify-center items-center gap-5 flex-wrap">
             <Card>
                 <BsFillPencilFill className="inline mr-2" />
             </Card>
             <Card background="bg-yellow-300" borderColor="border-green-400" fontColor="text-black" dividerColor="bg-black">
                 <BsFillPencilFill className="inline mr-2" />
             </Card>
+        </div>
        </HomeLayout>
     );
 }
