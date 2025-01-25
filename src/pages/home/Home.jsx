f801c3ff8355b96729fe048f01af13040502acb1
Reema Dhanwani <reemadhanwani1234@gmail.com>
2025-01-25 19:57:28 +0530

    Added reusable card component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,17 @@
+import { BsFillPencilFill } from "react-icons/bs";
+
+import Card from "../../components/Card";
 import HomeLayout from "../../layouts/HomeLayout";
 
 function Home() {
     return(
        <HomeLayout>
-            <h1>Hello</h1>
+            <Card>
+                <BsFillPencilFill className="inline mr-2" />
+            </Card>
+            <Card background="bg-yellow-300" borderColor="border-green-400" fontColor="text-black" dividerColor="bg-black">
+                <BsFillPencilFill className="inline mr-2" />
+            </Card>
        </HomeLayout>
     );
 }
