9f043dd6775fd70a478a7d01adc430e8bb3b1d78
Reema Dhanwani <reemadhanwani1234@gmail.com>
2025-01-27 21:10:57 +0530

    Added bar char data

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,136 +1,22 @@
-
-import { ArcElement, CategoryScale, Chart as ChartJS, Legend, LinearScale, LineElement,PointElement, Title,Tooltip } from "chart.js";
-import { useEffect,useState } from "react";
-import { Line, Pie } from 'react-chartjs-2';
+import { Bar,Line, Pie } from 'react-chartjs-2';
 import { BsFillPencilFill } from "react-icons/bs";
 import { MdCancel,MdOutlineDoneAll, MdPending } from 'react-icons/md';
 import { TbProgressBolt } from 'react-icons/tb';
 
 import Card from "../../components/Card";
+import useCharts from "../../hooks/useCharts";
 import useTickets from "../../hooks/useTickets";
 import HomeLayout from "../../layouts/HomeLayout";
 
 
-ChartJS.register(ArcElement, Legend, Title, Tooltip, CategoryScale, LinearScale, PointElement, LineElement);
+
 
 
 function Home() {
 
    const [ticketsState] = useTickets();
 
-   const [ticketsChartData, setTicketsChartData] = useState({
-    openTickets: [],
-    resolvedTickets: [],
-    inProgressTickets: []
-   });
-
-
-    function formatDate(dateStr) {
-        const [year, month, day] = dateStr.split('-').map(Number);
-        return `${year}-${day}-${month}`;
-    }
-   const pieChartData = {
-    labels: Object.keys(ticketsState.ticketDistribution),
-    fontColor: "white",
-    datasets: [
-        {
-            label: "Tickets data",
-            data: Object.values(ticketsState.ticketDistribution),
-            backgroundColor: ["yellow","red","green","blue","purple"],
-            borderColor: ["yellow","red","green","blue","purple"],
-            borderWidth: 1,
-        }
-    ]
-   };
-
-    const lineChartData = {
-        labels: Object.keys(ticketsChartData.openTickets),
-        fontColor: "white",
-        datasets: [
-            {
-                label: "Open Tickets data",
-                data: Object.values(ticketsChartData.openTickets),
-                borderColor: 'rgb(255, 99, 132)',
-            },
-            {
-                label: "Resolved Tickets data",
-                data: Object.values(ticketsChartData.resolvedTickets),
-                borderColor: 'rgb(245, 205, 95)',
-                borderWidth: 4
-            },
-            {
-                label: "In Progress Tickets data",
-                data: Object.values(ticketsChartData.inProgressTickets),
-                borderColor: 'rgb(53, 162, 235)',
-            },
-        ]
-    };
-
-    function processTickets() {
-        // Fetch the current Date
-        const currentDate = new Date(); 
-        // Calculate the 10th date from today
-        const tenthDayFromToday = new Date(); 
-        tenthDayFromToday.setDate(currentDate.getDate() - 10); 
-        // Process all the tickets
-        if(ticketsState.ticketList.length > 0 ) {
-            // Prepare two localobjects to act as frequency map
-            let openTicketsData = {};
-            let inProgressTicketsData = {};
-            let resolvedTicketsData = {};
-            // Initialise the frequency map with default value 0 for the last 10 days
-            for(let i = 0; i < 10; i++) {
-                // Get the ith day from today
-                const dateObject = new Date();
-                dateObject.setDate(currentDate.getDate() - i);
-                /**
-                 * dateObject.toLocaleDateString() -> gives us string in the format DD/MM/YYYY
-                 * Convert this to YYYY-MM-DD
-                 */
-                openTicketsData[dateObject.toLocaleDateString().split("/").reverse().join("-")] = 0;
-                inProgressTicketsData[dateObject.toLocaleDateString().split("/").reverse().join("-")] = 0;
-                resolvedTicketsData[dateObject.toLocaleDateString().split("/").reverse().join("-")] = 0;
-            }
-            // Process all the tickets one by one
-            ticketsState.ticketList.forEach((ticket) => {
-                // Get the date part from the tickets by removing everything post the character T
-                let date = ticket.createdAt.split("T")[0];
-                date = formatDate(date);
-                const ticketDate = new Date(ticket.createdAt);
-                console.log("date :" ,date,"Ticket date",ticketDate)
-                // If ticket is open and lies in the last 10 days add it
-                if(ticket.status == "open" && ticketDate >= tenthDayFromToday) {
-                    openTicketsData[date] =  openTicketsData[date] + 1;
-                    console.log("Open Ticket data", openTicketsData);
-                }
-                // If ticket is inProgress and lies in the last 10 days add it
-                if(ticket.status == "inProgress" && ticketDate >= tenthDayFromToday) {
-                    inProgressTicketsData[date] = inProgressTicketsData[date] + 1;
-                    console.log("inProgress Ticket data", inProgressTicketsData);
-                }
-                // If ticket is resolved and lies in the last 10 days add it
-                if(ticket.status == "resolved" && ticketDate >= tenthDayFromToday) {
-                    resolvedTicketsData[date] = resolvedTicketsData[date] + 1;
-                    console.log("resolve Ticket data", resolvedTicketsData);
-                }
-                if(ticket.status == "resolved") {
-                    console.log(ticket);
-                }
-            });
-            //  update the state
-            console.log(resolvedTicketsData);
-            setTicketsChartData({
-                openTickets: openTicketsData,
-                inProgressTickets: inProgressTicketsData,
-                resolvedTickets: resolvedTicketsData
-            });
-        }
-    }
-
-    useEffect(() => {
-        console.log("Ticket states",ticketsState);
-        processTickets();
-    }, [ticketsState.ticketList]);
+   const [pieChartData,lineChartData,barChartData] = useCharts();
     
     return(
        <HomeLayout>
@@ -205,7 +91,12 @@ function Home() {
                 <div className="w-[50rem]">
                     <Line data={lineChartData}/>
                 </div>
-            </div>
+        </div>
+        <div className="mt-10 mb-10 flex justify-center items-center gap-10">
+                <div className="w-[50rem]">
+                    <Bar data={barChartData}/>
+                </div>
+        </div>
        </HomeLayout>
     );
 }
