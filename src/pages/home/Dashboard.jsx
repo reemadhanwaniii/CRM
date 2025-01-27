80ecebb0f511dac3b44f3acf17b67b44c51e4516
Reema Dhanwani <reemadhanwani1234@gmail.com>
2025-01-27 12:14:39 +0530

    Added react-data-table component

--- src/pages/home/Dashboard.jsx
+++ src/pages/home/Dashboard.jsx
@@ -1,10 +1,13 @@
 
 
+import DataTable from 'react-data-table-component';
 import { AiOutlineDownload } from "react-icons/ai";
 import { usePDF } from "react-to-pdf";
 
 import useTickets from "../../hooks/useTickets";
 import HomeLayout from "../../layouts/HomeLayout";
+
+const ExpandedComponent = ({ data }) => <pre>{JSON.stringify(data, null, 2)}</pre>;
 
 function Dashboard() {
 
@@ -12,8 +15,67 @@ function Dashboard() {
 
     const { toPDF, targetRef } = usePDF({filename: 'page.pdf'});
 
-    
+
+    const columns = [
+        {
+            name: 'Ticket Id',
+            selector: row => row._id,
+            reorder: true,
+        },
+        {
+            name: 'Title',
+            selector: row => row.title,
+            reorder: true,
+        },
+        {
+            name: 'Description',
+            selector: row => row.description,
+            reorder: true,
+        },
+        {
+            name: 'Reporter',
+            selector: row => row.assignedTo,
+            reorder: true,
+        },
+        {
+            name: 'Priority',
+            selector: row => row.ticketPriority,
+            reorder: true,
+            sortable: true,
+        },
+        {
+            name: 'Assignee',
+            selector: row => row.assignee,
+            reorder: true,
+        },
+        {
+            name: 'Status',
+            selector: row => row.status,
+            reorder: true,
+            sortable: true,
+        }
+    ];
    
+    const customStyles = {
+        // rows: {
+        //     style: {
+        //         minHeight: '72px', // override the row height
+        //         fontSize: '18px'
+        //     },
+        // },
+        // headCells: {
+        //     style: {
+        //         paddingLeft: '8px', // override the cell padding for head cells
+        //         paddingRight: '8px',
+        //     },
+        // },
+        // cells: {
+        //     style: {
+        //         paddingLeft: '8px', // override the cell padding for data cells
+        //         paddingRight: '8px',
+        //     },
+        // },
+    };
 
     return(
         <HomeLayout>
@@ -23,60 +85,16 @@ function Dashboard() {
                 </div>
                 {/* Table */}
 
-                <div className="flex flex-col w-full" ref={targetRef}>
-                    {/* Title row */}
-                    <div className="my-4 flex justify-between items-center gap-3 bg-purple-600 px-2 py-2">
-                        <div className="table-title basis-[8%] justify-start">
-                            Ticket ID
-                        </div>
-                        <div className="table-title basis-[12%]">
-                            Title
-                        </div>
-                        <div className="table-title basis-[20%]">
-                            Description
-                        </div>
-                        <div className="table-title basis-[20%]">
-                            Reporter
-                        </div>
-                        <div className="table-title basis-[3%]">
-                            Priority
-                        </div>
-                        <div className="table-title basis-[22%]">
-                            Assignee
-                        </div>
-                        <div className="table-title basis-[13%] justify-end mr-4">
-                            Status
-                        </div>
-                    </div>
-
-                    {/* Ticket Details */}
-                        {ticketsState && ticketsState.ticketList.map((ticket) => {
-                            return(
-                            <div key={ticket._id} className="my-4 text-sm flex justify-between items-center gap-3 bg-gray-100 hover:bg-gray-400 text-black px-2 py-2">
-                                <div className="table-title basis-[8%] justify-start">
-                                    {ticket._id.substring(0,5)+".."}
-                                </div>
-                                <div className="table-title basis-[12%]">
-                                    {ticket.title}
-                                </div>
-                                <div className="table-title basis-[20%]">
-                                    {ticket.description}
-                                </div>
-                                <div className="table-title basis-[20%]">
-                                     {ticket.assignee}
-                                </div>
-                                <div className="table-title basis-[5%]">
-                                    {ticket.ticketPriority}
-                                </div>
-                                <div className="table-title basis-[22%]">
-                                    {ticket.assignedTo}
-                                </div>
-                                <div className="table-title basis-[13%] justify-end mr-4">
-                                    {ticket.status}    
-                                </div>
-                            </div>
-                            );
-                        })}
+                <div ref={targetRef}>
+                    {ticketsState && 
+                        <DataTable
+                            columns={columns}
+                            data={ticketsState.ticketList}
+                            expandableRows
+                            expandableRowsComponent={ExpandedComponent}
+                            customStyles={customStyles}
+                        />
+                    }
                 </div>
             </div>
         </HomeLayout>
